0abd3e294ed54b036ec2ed67c0de413f997b8a1b
Janiece Eleonor <janiecee@ev-g6.kzn.21-school.ru>
2024-04-17 15:23:06 +0300

    Solution brackets with stack

--- ValidParentheses/ValidParentheses/main.swift
+++ ValidParentheses/ValidParentheses/main.swift
@@ -8,83 +8,30 @@
 import Foundation
 
 class Solution {
-    enum Bracket {
-        case roundOpen, roundClose, squareOpen, squareClose, curlyOpen, curlyClose
-    }
-        
-    let brackets: [String.Element] = ["(", ")", "[", "]", "{", "}"]
-    var status = [Bracket: (Int, Int)]()
-    
     func isValid(_ s: String) -> Bool {
-        var result: Bool = true
+        let dictionary: Dictionary<Character, Character> = [
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        ]
+        var stack: Array<Character> = []
         
-        for (index, char) in s.enumerated() {
-            if !isBrackets(char) || !checkIndex() || !checkCount(index, s.count) {
-                return false
+        for char in s {
+            if char == "}" || char == ")" || char == "]" {
+                if stack.isEmpty || stack.last != dictionary[char] {
+                    return false
+                } else {
+                    stack.removeLast()
+                }
             } else {
-                updateStatus(index, char)
+                stack.append(char)
             }
         }
-        
-        return result
-    }
-    
-    func isBrackets(_ char: String.Element) -> Bool {
-        print(brackets.contains(char))
-        return brackets.contains(char)
-    }
-    
-    func isCloseBracketFirst(_ index: Int, _ char: String.Element) -> Bool {
-        let closeBrackets: [String.Element] = [")", "]", "}"]
-        return index == 0 && closeBrackets.contains(char)
-    }
-    
-    func updateStatus(_ index: Int,_ char: String.Element) {
-        switch char {
-        case "(":
-            status.updateValue((index, (status[.roundOpen]?.1 ?? 0) + 1), forKey: .roundOpen)
-        case ")":
-            status.updateValue((index, (status[.roundClose]?.1 ?? 0) + 1), forKey: .roundClose)
-        case "[":
-            status.updateValue((index, (status[.squareOpen]?.1 ?? 0) + 1), forKey: .squareOpen)
-        case "]":
-            status.updateValue((index, (status[.squareClose]?.1 ?? 0) + 1), forKey: .squareClose)
-        case "{":
-            status.updateValue((index, (status[.curlyOpen]?.1 ?? 0) + 1), forKey: .curlyOpen)
-        case "}":
-            status.updateValue((index, (status[.curlyClose]?.1 ?? 0) + 1), forKey: .curlyClose)
-        default:
-            return
-        }
-    }
-    
-    func checkIndex() -> Bool {
-        if let left = status[.roundOpen], let right = status[.roundClose] {
-            return left.0 < right.0
-        } else if let left = status[.squareOpen], let right = status[.squareClose] {
-            return left.0 < right.0
-        } else if let left = status[.curlyOpen], let right = status[.curlyClose] {
-            return left.0 < right.0
-        } else {
-            return true
-        }
-    }
-    
-    func checkCount(_ index: Int, _ count: Int) -> Bool {
-        if index == count - 1 {
-            if let left = status[.roundOpen], let right = status[.roundClose] {
-                return left.1 == right.1
-            } else if let left = status[.squareOpen], let right = status[.squareClose] {
-                return left.1 == right.1
-            } else if let left = status[.curlyOpen], let right = status[.curlyClose] {
-                return left.1 == right.1
-            } 
-        }
-        return true
+        return stack.isEmpty
     }
 }
 
 let solution = Solution()
-let string = "(((((((){}[]"
+let string = "[()()]"
 
 print(solution.isValid(string))
